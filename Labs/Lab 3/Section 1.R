eda58d9d03c22dda9703b0e9e99cca5cc1a6d099
Naman Modani <modani.naman@gmail.com>
2022-02-13 21:52:52 -0800

    Completed Section 1

--- Labs/Lab 3/Section 1.R
+++ Labs/Lab 3/Section 1.R
@@ -35,3 +35,37 @@ abline(soil_model, col = "red", lwd = 2)
 plot(soil_model$residuals ~ soil$zinc,
      main = "Residuals plot")
 abline(a = 0, b = 0, col = "red", lwd = 2)
+
+# 1D
+
+## Lead Concentration = 16.582928 + 0.291335 * Zinc Concentration
+
+# 1E
+
+16.582928 + 0.291335 * 1000
+
+# 1F
+
+zinc_A = 100 + zinc_B
+lead_A - lead_B = (16.582928 + 0.291335 * zinc_A) - (16.582928 + 0.291335 * zinc_B)
+                = 0.291335 * (zinc_A - zinc_B)
+                = 0.291335 * 100
+                = 29.1335
+
+# 1G
+
+## R-squared is 0.912. This means that 91.2% of the variation in lead 
+## concentration can be explained by the zinc concentration.
+
+# 1H
+
+## Linearity Assumption: Since most of the data is close to the regression
+## line, the linearity assumption holds good.
+
+## Symmetry Assumption: Albeit fairly subjective, the symmetry is good since 
+## roughly the same # of positive and negative residuals are on both sides of the 
+## line.
+
+## Variance Assumption: Clearly violated, since the x-value is directly 
+## proportional to variance. As we traverse along the x-axis, we see an increase
+## in residual variance.
