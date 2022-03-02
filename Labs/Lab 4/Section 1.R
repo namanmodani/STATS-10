2c8dbe398345d3bbbac45012fd589fd65ff42907
Naman Modani <modani.naman@gmail.com>
2022-03-01 17:23:27 -0800

    Section 1 Cleanup

--- Labs/Lab 4/Section 1.R
+++ Labs/Lab 4/Section 1.R
@@ -20,13 +20,17 @@ sqrt(n * p * (1 - p))
 
 # 1C
 
+## P(park will experience exactly 145 days of heavy rain in 2019)
 dbinom(145, size = n, prob = p)
 
 # 1D
 
+## P(park will see between 125 and 175 days of heavy rain in 2019)
 pbinom(175, size = n, prob = p) - pbinom(124, size = n, prob = p)
 
 # 1E
+
+## P(park will experience more than 230 inches of rain in 2019)
 
 ## Method 1
 1 - pnorm(230, mean = 200, sd = 20)
