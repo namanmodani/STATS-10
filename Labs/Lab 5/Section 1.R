f716c510aef68af7338527fc570df812d6b3d98a
Naman Modani <modani.naman@gmail.com>
2022-03-05 10:24:10 -0800

    Solved problem

--- Labs/Lab 5/Section 1.R
+++ Labs/Lab 5/Section 1.R
@@ -2,7 +2,7 @@
 # Lab 5 Section 1
 # Naman Modani
 
-# 5A
+# 1A
 
 flint <- read.csv("flint_2015.csv", header = TRUE)
 
@@ -10,7 +10,16 @@ flint <- read.csv("flint_2015.csv", header = TRUE)
 ## H1: p > 0.1
 ## Since we have a greater than/less than sign, the hypothesis is one-sided.
 
-# 5B
+# 1B
 
 p_hat <- mean(flint$Pb >= 15)
 p_hat
+n <- nrow(flint)
+sqrt(p_hat * (1 - p_hat) / n)
+
+# 1C
+p0 = 0.1
+se = sqrt(p0 * (1 - p0) / n)
+se
+z_stat = (p_hat - p0) / se
+z_stat
