3581f48619b6698ba7a31db8ec5b4ec72b18b8d3
Naman Modani <modani.naman@gmail.com>
2022-03-05 10:45:02 -0800

    Update Section 1.R

--- Labs/Lab 5/Section 1.R
+++ Labs/Lab 5/Section 1.R
@@ -18,8 +18,34 @@ n <- nrow(flint)
 sqrt(p_hat * (1 - p_hat) / n)
 
 # 1C
+
 p0 = 0.1
 se = sqrt(p0 * (1 - p0) / n)
 se
 z_stat = (p_hat - p0) / se
 z_stat
+
+# 1D
+
+1 - pnorm(z_stat)
+pnorm(z_stat, lower.tail = FALSE)
+
+# 1E
+
+## Yes, we reject the null hypothesis since the p-value is lower than the
+## significance level.
+
+# 1F
+
+## The EPA should taken remediation action immediately since greater than 10% 
+## of households in Flint contain dangerous lead levels. Since we reject the 
+## null hypothesis, we are in favor of the alternate hypothesis.
+
+# 1G
+
+library(mosaic)
+prop.test(x = sum(flint$Pb >= 15), n = nrow(flint), p = 0.1, alt = "greater")
+
+## The p-value is 0.03778.
+## No, the results do not change. This is because we still reject the null
+## hypothesis, since the p-value 0.03778 < 0.05.
