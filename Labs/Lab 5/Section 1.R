994f35a8aac03cfb5c4a7089c0eb54f880be67e6
Naman Modani <modani.naman@gmail.com>
2022-03-05 10:15:35 -0800

    Update Section 1.R

--- Labs/Lab 5/Section 1.R
+++ Labs/Lab 5/Section 1.R
@@ -4,4 +4,13 @@
 
 # 5A
 
+flint <- read.csv("flint_2015.csv", header = TRUE)
+
+## H0: p = 0.1
+## H1: p > 0.1
+## Since we have a greater than/less than sign, the hypothesis is one-sided.
+
 # 5B
+
+p_hat <- mean(flint$Pb >= 15)
+p_hat
